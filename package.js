d4d23ea05e0c38255d55481ed5c30baa3e95af35
paul <xvendo@gmail.com>
2016-11-10 19:09:26 +0100

    Package source changed

---
+++ package.js
@@ -0,0 +1,18 @@
+Package.describe({
+  name: 'xvendo:buttonjs',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'Implements tagit JQUERY Plugin',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
+  api.use('ecmascript');
+  api.use('mizzao:jquery-ui@1.11.4');
+  api.addFiles(['src/button.js'], 'client');  
+});

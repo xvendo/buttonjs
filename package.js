0e9b5af81f57a7b7bbfe110ab1d8e44ac8d18698
paul <xvendo@gmail.com>
2016-11-11 00:24:58 +0100

    New versions added

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'xvendo:buttonjs',
-  version: '0.0.1',
+  version: '1.0.0',
   // Brief, one-line summary of the package.
   summary: 'Implements tagit JQUERY Plugin',
   // URL to the Git repository containing the source code for this package.
